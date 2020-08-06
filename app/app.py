f43f96d73fe07b229215a769cf28fa1eded6d356
anis <anisshili.as@gmail.com>
2020-08-06 07:48:52 -0400

    Task: added calendar page and fixed README.MD

--- app/app.py
+++ app/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/calendar')
+def main():
+    return render_template('calendar.html')
+
 
 if __name__ == ' __main__':
     app.run(debug=True)
